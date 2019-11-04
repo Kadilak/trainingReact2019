c8d144c0181019463b3b6cea9c27d70cd7a33b91
Kamil Obst <obst.kamil@gmail.com>
2019-11-04 18:01:59 +0100

    Add initial stuff

---
+++ auction/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="jumbotron">
+      <h1>Lets get starteddd</h1>
+      <h1>Lets get starteddd</h1>
+      <h1>Lets get starteddd</h1>
+      <h1>Lets get starteddd</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
